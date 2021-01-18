48c716f48aa3d3e35520019792dd4fb10bbd5ade
bigsun16 <bigsun16@126.com>
2021-01-18 16:14:46 +0800

    test stream

--- study_jdk/src/main/java/com/qihui/sun/jdk8/TestStream.java
+++ study_jdk/src/main/java/com/qihui/sun/jdk8/TestStream.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TestStream {
@@ -73,5 +75,17 @@ public class TestStream {
             }
 
         }).forEach(System.out::println);
+    }
+
+    @Test
+    public void test4(){
+        List<Employee> collect = list.stream()
+                .collect(Collectors.groupingBy(Employee::getStatus))
+                .values().stream()
+                .map(employees -> employees.stream()
+                        .max(Comparator.comparing(Employee::getSalary))
+                        .get()
+                ).collect(Collectors.toList());
+        System.out.println(collect);
     }
 }
